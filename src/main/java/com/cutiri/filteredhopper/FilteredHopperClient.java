20bba7d09b938a94e574cd03cb4a7296e5f35f4a
Angel Villasante <avillasante@hotmail.com>
2023-08-29 22:14:37 -0700

    Initial commit

---
+++ src/main/java/com/cutiri/filteredhopper/FilteredHopperClient.java
@@ -0,0 +1,29 @@
+package com.cutiri.filteredhopper;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class FilteredHopperClient implements ClientModInitializer {
+
+    // fields region
+    //region Fields
+    //endregion
+
+    // constructors region
+    //region Constructors
+    //endregion
+
+    // methods region
+    //region Methods
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+    //endregion
+
+    // setters and getters region
+    //region setters and getters
+    //endregion
+}
